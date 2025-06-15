78a57b2cc529a6c3fb95e00d233188c79c278b66
Luisguialfa <luisguialfa@gmail.com>
2025-06-15 02:02:50 -0600

    ventana principal lista

--- funciones.py
+++ funciones.py
@@ -75,10 +75,6 @@ def crearInventario():
         infoAnimal.setInformacion(estado,calificacion,x[1][1],78)
         conta+=1
         lstAnimal.append(infoAnimal)
-<<<<<<< HEAD
-=======
     grabar(lstAnimal,"laLista")
-    print(lstAnimal)
 
->>>>>>> a6cb3ab2cd4a24fd5e59b392b4ab05a75e090be3
 crearInventario()
--- interfaz.py
+++ interfaz.py
@@ -11,23 +11,30 @@ def configVtnPrincipal():
     Salidas:
     - Muestra el menú principal del programa.
     """
-    vtnPrincipal.state("zoomed")
     vtnPrincipal.configure(bg="#3C5520")
     vtnPrincipal.title("Zoo Inventario")
+    vtnPrincipal.geometry("1090x850+0+0")
     vtnPrincipal.iconbitmap("imagenes/icono.ico")
-    imgBtn=PhotoImage(file="imagenes/plantillaBotones.png")
+    vtnPrincipal.resizable(0,0)
+    imgFondo=PhotoImage(file="imagenes/fondoPrincipal.png")
+    imgBtn=PhotoImage(file="imagenes/btnObtenerLista.png")
+
+    lblFondo=Label(vtnPrincipal,image=imgFondo)
     btn=Button(vtnPrincipal,image=imgBtn,borderwidth=0, highlightthickness=0,bg="#3C5520",activebackground="#3C5520")
-    btn.image_names=imgBtn
     btn1=Button(vtnPrincipal,image=imgBtn,borderwidth=0, highlightthickness=0,bg="#3C5520",activebackground="#3C5520")
-    btn1.image_names=imgBtn
     btn2=Button(vtnPrincipal,image=imgBtn,borderwidth=0, highlightthickness=0,bg="#3C5520",activebackground="#3C5520")
-    btn2.image_names=imgBtn
     btn3=Button(vtnPrincipal,image=imgBtn,borderwidth=0, highlightthickness=0,bg="#3C5520",activebackground="#3C5520")
+    btn.image_names=imgBtn
+    btn1.image_names=imgBtn
+    btn2.image_names=imgBtn
     btn3.image_names=imgBtn
-    btn.place(x=100, y=200)
-    btn1.place(x=100, y=400)
-    btn2.place(x=600, y=200)
-    btn3.place(x=600, y=400)
+    lblFondo.image_names=imgFondo
+    lblFondo.place(x=-2,y=-2)
+    btn.place(x=206, y=235)
+    btn1.place(x=206, y=393)
+    btn2.place(x=604, y=235)
+    btn3.place(x=604, y=393)
+
 
 configVtnPrincipal()
 vtnPrincipal.mainloop()
