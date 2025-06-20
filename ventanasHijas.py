a9bdaa9ee5c61ba5f71ce60578a586f865a07109
Luisguialfa <luisguialfa@gmail.com>
2025-06-19 18:23:35 -0600

    ventanaMstInv

--- ventanasHijas.py
+++ ventanasHijas.py
@@ -9,3 +9,12 @@ def ventanaEstxEstado():
     vtnEstxEstado.geometry("1090x850+0+0")
     vtnEstxEstado.iconbitmap("imagenes/icono.ico")
     vtnEstxEstado.resizable(0,0)
+
+def ventanaMstInv():
+    cantidades=estaXEstado()
+    vtnEstxEstado= Toplevel()
+    vtnEstxEstado.configure(bg="#3C5520")
+    vtnEstxEstado.title("Estadistica")
+    vtnEstxEstado.geometry("1090x850+0+0")
+    vtnEstxEstado.iconbitmap("imagenes/icono.ico")
+    vtnEstxEstado.resizable(0,0)
